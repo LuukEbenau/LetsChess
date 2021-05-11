222a31a6daf88b795d6516f97e96174ca259a0fd
LuukEbenau <l.ebenau@hotmail.com>
2021-05-11 22:31:00 +0200

    setup kubernetes for front, back and userservice

---
+++ LetsChess-GameService/Controllers/GameController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LetsChess_GameService.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class GameController : ControllerBase
+	{
+		private readonly ILogger<GameController> _logger;
+
+		public GameController(ILogger<GameController> logger)
+		{
+			_logger = logger;
+		}
+
+		[HttpGet("takemove")]
+		public IActionResult TakeMove()
+		{
+			return Ok();
+		}
+	}
+}
---
+++ LetsChess-GameService/Logic/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LetsChess_GameService.Logic
+{
+	public class Game
+	{
+		public void TakeMove(string from, string to) { 
+			//TODO: rabbitmq etc
+		}
+	}
+}
