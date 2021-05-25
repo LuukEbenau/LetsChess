53b5e79b921f1faa0b6d158b006288dde8ccdd9e
LuukEbenau <l.ebenau@hotmail.com>
2021-05-25 23:27:39 +0200

    config files

--- start-app.bat
+++ start-app.bat
@@ -1,3 +1,5 @@
-docker-compose up -d
+echo "this is needed for a weird bug"
+wsl -d docker-desktop -e sysctl -w vm.max_map_count=262144
+docker compose up -d
 cd LetsChess-Front/app
 yarn start
---
+++ update-services.bat
@@ -0,0 +1,9 @@
+echo lets update all services!
+color 01
+./LetsChess-Backend/update-service.bat
+color 02
+./LetsChess-MatchmakingService/update-service.bat
+color 03
+./LetsChess-UserService/update-service.bat
+color 04
+./LetsChess-Front/update-service.bat
