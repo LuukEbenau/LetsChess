62c4fb7e9c348d8076ec459ff921ceef3613a923
LuukEbenau <l.ebenau@hotmail.com>
2021-06-01 18:46:36 +0200

    models, for now in the main repo

---
+++ LetsChess.Models/Messages/TakeMoveMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LetsChess_Models.Models
+{
+	public class TakeMoveMessage
+	{
+		public string MatchId { get; set; }
+		public string UserId { get; set; }
+		public string From { get; set; }
+		public string To { get; set; }
+	}
+}
